862fe3adef73d5dcf76c5b7441daf3c77859816d
vanleantking <van.le.k3c1@gmail.com>
2019-04-03 13:14:39 +0700

    --bk

--- apps/backend/controllers/WebConfigController.php
+++ apps/backend/controllers/WebConfigController.php
@@ -12,6 +12,10 @@ class WebConfigController extends BaseController {
         parent::initialize();
     }
 	public function indexAction() {
+		$webConfig = WebConfig::find(
+			'condition' => array(),
+			'sort'
+		)
 		var_dump('expression');
 	}
 
@@ -23,54 +27,57 @@ class WebConfigController extends BaseController {
 			if ($this->security->checkToken()) {
 				$arrPost = $this->request->getPost();
 				if ($configform->isValid($arrPost)) {
-					var_dump(WebConfig::find(
-						[
-							[
-								'URL' => "https://vnexpress.net/kinh-doanh"
-							]
-						]
-					));exit;
-					$webConfig = new WebConfig();
-					$webConfig->domain = $arrPost["domain"];
-					$webConfig->URL = $arrPost["url"];
-					$webConfig->list_news = $arrPost["list_news"];
-					$webConfig->title_news = $arrPost["title_news"];
-					$webConfig->paginate_rexp = $arrPost["paginate_rexp"];
-					$webConfig->homepage = isset($arrPost["homepage"]) ? true : false;
-					$webConfig->content_class = $arrPost["content_class"];
-					$webConfig->category_class = $arrPost["category_class"];
-					$webConfig->special_header = isset($arrPost["special_header"]) ? true : false;
-					$webConfig->meta_description = $arrPost["meta_description"];
-					$webConfig->meta_keyword = $arrPost["meta_keyword"];
-					$webConfig->category = $arrPost["category"];
-					$webConfig->meta = $arrPost["meta"];
-					$webConfig->comments_class = $arrPost["comments_class"];
-					$webConfig->get_comment = isset($arrPost["get_comment"]) ? true : false;
-					// var_dump($webConfig);exit;
-				    if ($webConfig->save()) {
-				    	$this->flash->success("Add success!");
-				    	return $this->response->redirect(ADMIN_URL.'webconfig/index');
-				    }
-				    $this->flash->error("Add fail!");
+					$isExist = $this->checkConfigExist($arrPost['url']);
+
+					if(!$isExist['is_exist']){
+						$webConfig = new WebConfig();
+						$webConfig->domain = $arrPost["domain"];
+						$webConfig->url = $arrPost["url"];
+						$webConfig->validate_url = $isExist['validate_url'];
+						$webConfig->list_news = $arrPost["list_news"];
+						$webConfig->title_news = $arrPost["title_news"];
+						$webConfig->paginate_rexp = $arrPost["paginate_rexp"];
+						$webConfig->homepage = isset($arrPost["homepage"]) ? true : false;
+						$webConfig->content_class = $arrPost["content_class"];
+						$webConfig->category_class = $arrPost["category_class"];
+						$webConfig->special_header = isset($arrPost["special_header"]) ? true : false;
+						$webConfig->meta_description = $arrPost["meta_description"];
+						$webConfig->meta_keyword = $arrPost["meta_keyword"];
+						$webConfig->category = $arrPost["category"];
+						$webConfig->meta = $arrPost["meta"];
+						$webConfig->comments_class = $arrPost["comments_class"];
+						$webConfig->get_comment = isset($arrPost["get_comment"]) ? true : false;
+					    if ($webConfig->save()) {
+					    	$this->flash->success("Add success!");
+					    	return $this->response->redirect('webconfig/index');
+					    }
+					    $this->flash->error("Add fail!");
+					
+					} else {
+						$this->flash->error("Config exist");
+					}
 				}
 			}
 		}
 		$this->view->form = $configform;
 	}
 
-	public function checkConfigExist($data) {
+	private function checkConfigExist($data) {
 		$url = $this->processURL($data['url']);	
-
 		$isExist = WebConfig::find(
 			[
 				[
-					'url' => $url
+
+					'validate_url' => $url
 				]
 			]
 		);
+		return array(
+			'is_exist' => $isExist,
+			'validate_url' => $url)
 	}
 
-	public function processURL($url) {
+	private function processURL($url) {
 		$arr_process = array('https://', 'http://', 'www.', '#');
 		foreach ($arr_process as $str) {
 			if ($str == "#") {
@@ -81,8 +88,10 @@ class WebConfigController extends BaseController {
 		}
 		$len_url = strlen($url);
 		if (substr($url, -1) == "/") {
-			$url = substr($url, 0, $len_url-2);
+			$url = substr($url, 0, $len_url-1);
 		}
+
+		return $url;
 	}
 
 
